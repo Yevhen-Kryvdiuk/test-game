a372a8daad8f9bc3a3cd832f3929b2f3ae828c08
Yevhen <yevhen@MacBook-Pro-Yevhen.local>
2026-04-10 17:38:44 +0300

    Y-1: init front + server

---
+++ front/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Duck Hunt</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: 'http://localhost:5173' }
+});
+
+io.on('connection', (socket) => {
+  console.log('connected', socket.id);
+});
+
+server.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
